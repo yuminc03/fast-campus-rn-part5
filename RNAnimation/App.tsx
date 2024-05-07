3ee8e07aca6b86eb365106af86a176c100733857
Yumin Chu <yuminc03@gmail.com>
2024-05-07 13:38:11 +0900

    Feat: Animated.timing - Easing 사용하기

--- RNAnimation/App.tsx
+++ RNAnimation/App.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import {SafeAreaView} from 'react-native';
+import AnimatedCoomponents from './src/chapter2/AnimatedComponents';
 import AnimatedValue from './src/chapter2/AnimatedValue';
+import AninatedTiming from './src/chapter2/AnimatedTiming';
 
 const App = () => {
   return (
     <SafeAreaView
       style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
-      <AnimatedValue />
+      <AninatedTiming />
     </SafeAreaView>
   );
 };
---
+++ RNAnimation/src/chapter2/AnimatedTiming.js
@@ -0,0 +1,28 @@
+import React, {useRef} from 'react';
+import {Animated, Button, Text, Easing} from 'react-native';
+
+export default function AnimatedTiming() {
+  const translateXAnim = useRef(new Animated.Value(-100)).current;
+
+  // Easing ease  back/ bounce /elastic / circle
+  const onButtonPress = () => {
+    translateXAnim.setValue(-100);
+    Animated.timing(translateXAnim, {
+      toValue: 100,
+      duration: 1000,
+      delay: 0,
+      easing: Easing.out(Easing.circle),
+      useNativeDriver: true,
+    }).start();
+  };
+
+  return (
+    <>
+      <Button title="움직임" onPress={onButtonPress} />
+      <Animated.Text
+        style={{fontSize: 70, transform: [{translateX: translateXAnim}]}}>
+        🍊
+      </Animated.Text>
+    </>
+  );
+}
