da68211920af66f90bc5178123a658b1d197c7cd
Yumin Chu <yuminc03@gmail.com>
2024-05-08 14:28:53 +0900

    Feat: Skeleton Animation

--- RNAnimation/src/chapter3/Skeleton.js
+++ RNAnimation/src/chapter3/Skeleton.js
@@ -1,7 +1,20 @@
 import React, {useEffect, useRef} from 'react';
-import {Animated, View} from 'react-native';
+import {Animated, View, Text} from 'react-native';
+import LinearGradient from 'react-native-linear-gradient';
 
 export default function Skeleton() {
+  const interpolateAnim = useRef(new Animated.Value(0)).current;
+
+  useEffect(() => {
+    Animated.loop(
+      Animated.timing(interpolateAnim, {
+        toValue: 1,
+        duration: 1200,
+        useNativeDriver: false,
+      }),
+    ).start();
+  });
+
   return (
     <View
       style={{
@@ -12,7 +25,14 @@ export default function Skeleton() {
       }}>
       {[...Array(8)].map((value, index) => {
         return (
-          <View style={{flexDirection: 'row', marginVertical: 8}} key={index}>
+          <View
+            style={{
+              flexDirection: 'row',
+              marginVertical: 8,
+              position: 'relative',
+              overflow: 'hidden',
+            }}
+            key={index}>
             <View
               style={{
                 width: 60,
@@ -49,6 +69,24 @@ export default function Skeleton() {
                 }}
               />
             </View>
+
+            <Animated.View
+              style={{
+                position: 'absolute',
+                top: -30,
+                left: interpolateAnim.interpolate({
+                  inputRange: [0, 1],
+                  outputRange: ['-20%', '100%'],
+                }),
+                transform: [{rotate: '20deg'}],
+              }}>
+              <LinearGradient
+                start={{x: 0, y: 0}}
+                end={{x: 1, y: 0}}
+                colors={['#ffffff00', '#ffffff90', '#ffffff00']}>
+                <View style={{width: 40, height: 100}} />
+              </LinearGradient>
+            </Animated.View>
           </View>
         );
       })}
