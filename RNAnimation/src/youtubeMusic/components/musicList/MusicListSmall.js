4c3374b339c43932364ae553dafa75c20e4709b3
Yumin Chu <yuminc03@gmail.com>
2024-05-13 13:23:00 +0900

    Feat: small music list에 paging 효과 animation 추가

--- RNAnimation/src/youtubeMusic/components/musicList/MusicListSmall.js
+++ RNAnimation/src/youtubeMusic/components/musicList/MusicListSmall.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useRef} from 'react';
 import {View, Text, Image, ScrollView, Dimensions} from 'react-native';
 import Icon from 'react-native-vector-icons/Entypo';
 import {faker} from '@faker-js/faker';
@@ -6,17 +6,77 @@ import {faker} from '@faker-js/faker';
 const {width} = Dimensions.get('window');
 
 export default function MusicListSmall() {
+  const scrollStartRef = useRef();
+  const scrollRef = useRef();
+  const pageRef = useRef(1);
+
   return (
     <View>
       <Title />
       <ScrollView
+        ref={scrollRef}
         horizontal={true}
-        contentContainerStyle={{paddingHorizontal: 10}}>
+        contentContainerStyle={{paddingHorizontal: 10}}
+        scrollEventThrottle={1}
+        onScrollBeginDrag={e => {
+          const x = e.nativeEvent.contentOffset.x;
+          scrollStartRef.current = x;
+        }}
+        onScroll={e => {
+          const x = e.nativeEvent.contentOffset.x;
+          const dx = x - scrollStartRef.current;
+          console.log(dx);
+        }}
+        onScrollEndDrag={e => {
+          const x = e.nativeEvent.contentOffset.x;
+          const dx = x - scrollStartRef.current;
+
+          // 오른쪽 page로 붙는 애니메이션
+          if (dx > width / 4 && pageRef.current !== 3) {
+            //다음페이지로 넘어감
+            scrollRef.current?.scrollTo({
+              x: width * 0.92 * pageRef.current,
+              y: 0,
+              animated: true,
+            });
+
+            pageRef.current = pageRef.current + 1;
+            console.log('다음 페이지로 넘어가기');
+          }
+
+          // 기존 페이지에 머물기
+          if (dx > 0 && dx < width / 4) {
+            scrollRef.current?.scrollTo({
+              x: width * 0.92 * (pageRef.current - 1),
+              animated: true,
+            });
+
+            console.log('머물기');
+          }
+
+          // 왼쪽 page로 넘어가는 애니메이션
+          if (dx < -width / 4 && pageRef.current !== 1) {
+            scrollRef.current?.scrollTo({
+              x: width * 0.92 * (pageRef.current - 2),
+              animated: true,
+            });
+
+            pageRef.current = pageRef.current - 1;
+            console.log('왼쪽 페이지로 넘어가기');
+          }
+
+          if (-width / 4 < dx && dx < 0) {
+            scrollRef.current?.scrollTo({
+              x: width * 0.92 * (pageRef.current - 1),
+              animated: true,
+            });
+          }
+        }}>
         {[...Array(3)].map((val, idx) => {
           return (
             <View key={idx} style={{width: width * 0.92}}>
               {[...Array(4)].map((value, index) => {
-                return <MusicListSmallItem />;
+                return <MusicListSmallItem key={index} />;
               })}
             </View>
           );
