25773f2b752820137c66ac05725c47c7a37d6dd3
Yumin Chu <yuminc03@gmail.com>
2024-05-14 15:54:21 +0900

    Feat: 카드를 숨기고 다시 가져오는 x축 slide Animation

--- RNAnimation/src/mobilePay/MobilePay.js
+++ RNAnimation/src/mobilePay/MobilePay.js
@@ -1,81 +1,111 @@
-import React, {useRef} from 'react';
+import React, {useRef, useState} from 'react';
 import {View, Dimensions, PanResponder, Animated} from 'react-native';
 
-const card = [
-  {color: '#aaa'},
-  {color: '#bbb'},
-  {color: '#ccc'},
-  {color: '#ddd'},
-  {color: '#eee'},
-  {color: '#f2f2f2'},
-];
 const {width, height} = Dimensions.get('window');
 
 export default function MobilePay() {
+  const [focus, setFocus] = useState(5);
   const cardRef = useRef('fold'); // fold, unfold
   const yAnim = useRef(new Animated.Value(0)).current;
   const rotateZAnim = useRef(new Animated.Value(0)).current;
+  const card = [
+    {color: '#aaa', xAnim: useRef(new Animated.Value(0)).current},
+    {color: '#bbb', xAnim: useRef(new Animated.Value(0)).current},
+    {color: '#ccc', xAnim: useRef(new Animated.Value(0)).current},
+    {color: '#ddd', xAnim: useRef(new Animated.Value(0)).current},
+    {color: '#eee', xAnim: useRef(new Animated.Value(0)).current},
+    {color: '#f2f2f2', xAnim: useRef(new Animated.Value(0)).current},
+  ];
 
-  const panResponder = useRef(
-    PanResponder.create({
-      onMoveShouldSetPanResponder: () => true,
-      onPanResponderMove: (event, gestureState) => {
-        const {dy, dx} = gestureState;
-        // dy, dx 중 무엇이 더 크게 변했을까?
-        const XSlider = Math.abs(dy) < Math.abs(dx);
-        const YSlider = Math.abs(dx) < Math.abs(dy);
-
-        if (YSlider) {
-          // drag를 아래로
-          if (dy > 5 && dy < 100 && cardRef.current === 'fold') {
-            yAnim.setValue(dy);
-          }
-
-          if (dy > 5 && dy < 100 && cardRef.current === 'unfold') {
-            rotateZAnim.setValue(dy);
-          }
-
-          // drag를 위로
-          if (dy > -75 && dy < 5 && cardRef.current === 'unfold') {
-            yAnim.setValue(65 + dy);
-          }
+  const panResponder = PanResponder.create({
+    onMoveShouldSetPanResponder: () => true,
+    onPanResponderMove: (event, gestureState) => {
+      const {dy, dx} = gestureState;
+      // dy, dx 중 무엇이 더 크게 변했을까?
+      const XSlider = Math.abs(dy) < Math.abs(dx);
+      const YSlider = Math.abs(dx) < Math.abs(dy);
+      if (XSlider) {
+        // 카드 버리기
+        if (dx < -5 && cardRef.current === 'fold' && focus >= 0) {
+          card[focus].xAnim.setValue(dx);
         }
-      },
-      onPanResponderEnd: (event, gestureState) => {
-        const {dy, dx} = gestureState;
-        const XSlider = Math.abs(dy) < Math.abs(dx);
-        const YSlider = Math.abs(dx) < Math.abs(dy);
+      }
 
-        if (YSlider) {
-          if (dy > 5) {
-            Animated.spring(yAnim, {
-              toValue: 65,
-              duration: 300,
-              useNativeDriver: false,
-            }).start();
-            cardRef.current = 'unfold';
-          }
-
-          if (dy > 5 && cardRef.current === 'unfold') {
-            Animated.spring(rotateZAnim, {
-              toValue: 0,
-              duration: 300,
-              useNativeDriver: false,
-            }).start();
-          }
-
-          if (dy < -5) {
-            Animated.spring(yAnim, {
-              toValue: 0,
-              duration: 300,
-              useNativeDriver: false,
-            }).start();
-            cardRef.current = 'fold';
-          }
+      if (YSlider) {
+        // drag를 아래로
+        if (dy > 5 && dy < 100 && cardRef.current === 'fold') {
+          yAnim.setValue(dy);
         }
-      },
-    }),
-  ).current;
+
+        if (dy > 5 && dy < 100 && cardRef.current === 'unfold') {
+          rotateZAnim.setValue(dy);
+        }
+
+        // drag를 위로
+        if (dy > -75 && dy < 5 && cardRef.current === 'unfold') {
+          yAnim.setValue(65 + dy);
+        }
+      }
+    },
+    onPanResponderEnd: (event, gestureState) => {
+      const {dy, dx} = gestureState;
+      const XSlider = Math.abs(dy) < Math.abs(dx);
+      const YSlider = Math.abs(dx) < Math.abs(dy);
+      if (XSlider) {
+        if (dx < -5 && cardRef.current === 'fold' && focus >= 0) {
+          Animated.timing(card[focus].xAnim, {
+            toValue: -600,
+            duration: 300,
+            useNativeDriver: false,
+          }).start(({finished}) => {
+            if (finished) {
+              setFocus(el => el - 1);
+            }
+          });
+        }
+
+        if (dx > 5 && cardRef.current === 'fold' && focus < 5) {
+          Animated.timing(card[focus + 1].xAnim, {
+            toValue: 0,
+            duration: 300,
+            useNativeDriver: false,
+          }).start(({finished}) => {
+            if (finished) {
+              setFocus(el => el + 1);
+            }
+          });
+        }
+      }
+
+      if (YSlider) {
+        if (dy > 5) {
+          Animated.spring(yAnim, {
+            toValue: 65,
+            duration: 300,
+            useNativeDriver: false,
+          }).start();
+          cardRef.current = 'unfold';
+        }
+
+        if (dy > 5 && cardRef.current === 'unfold') {
+          Animated.spring(rotateZAnim, {
+            toValue: 0,
+            duration: 300,
+            useNativeDriver: false,
+          }).start();
+        }
+
+        if (dy < -5) {
+          Animated.spring(yAnim, {
+            toValue: 0,
+            duration: 300,
+            useNativeDriver: false,
+          }).start();
+          cardRef.current = 'fold';
+        }
+      }
+    },
+  });
 
   return (
     <View
@@ -101,6 +131,7 @@ export default function MobilePay() {
               style={{
                 transform: [
                   {translateY: translateY},
+                  {translateX: value.xAnim},
                   {
                     rotateZ: rotateZAnim.interpolate({
                       inputRange: [0, 20],
