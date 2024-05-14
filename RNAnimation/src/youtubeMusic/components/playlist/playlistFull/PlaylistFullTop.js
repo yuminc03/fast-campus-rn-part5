c5494fb2dfc2411bc4654d0b43f42faadb7efdbc
Yumin Chu <yuminc03@gmail.com>
2024-05-14 09:28:10 +0900

    Feat: playlist FullScreen Top UI

--- RNAnimation/src/youtubeMusic/components/playlist/playlistFull/PlaylistFullTop.js
+++ RNAnimation/src/youtubeMusic/components/playlist/playlistFull/PlaylistFullTop.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import {View, Text, Animated, Dimensions} from 'react-native';
 
+import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
+
 const {width, height} = Dimensions.get('window');
 
 export default function PlayListFullTop({playlistAnim}) {
@@ -8,7 +10,7 @@ export default function PlayListFullTop({playlistAnim}) {
     <Animated.View
       style={{
         height: playlistAnim.interpolate({
-          inputRange: [0, height / 2, height],
+          inputRange: [0, height / 2.5, height],
           outputRange: [0, 0, 100],
         }),
         opacity: playlistAnim.interpolate({
@@ -16,9 +18,62 @@ export default function PlayListFullTop({playlistAnim}) {
           outputRange: [0, 1],
         }),
       }}>
-      <View style={{width: width}}>
-
-      </View>
+      <Animated.View
+        style={{
+          width: playlistAnim.interpolate({
+            inputRange: [0, height / 2.5, height],
+            outputRange: [0, 0, width],
+          }),
+          marginLeft: -width * 0.1,
+          flexDirection: 'row',
+          justifyContent: 'space-between',
+          alignItems: 'center',
+        }}>
+        <View style={{flexDirection: 'row'}}>
+          <IconItem name="chevron-down" />
+          <IconItem />
+        </View>
+        <View
+          style={{
+            backgroundColor: '#111',
+            height: 30,
+            flexDirection: 'row',
+            alignItems: 'center',
+            borderRadius: 100,
+          }}>
+          <View
+            style={{
+              backgroundColor: '#333',
+              height: 30,
+              paddingHorizontal: 12,
+              justifyContent: 'center',
+              borderRadius: 100,
+            }}>
+            <Text style={{color: '#ffffff90', fontSize: 12}}>노래</Text>
+          </View>
+          <View style={{paddingHorizontal: 10}}>
+            <Text style={{color: '#ffffff90', fontSize: 12}}>동영상</Text>
+          </View>
+        </View>
+        <View style={{flexDirection: 'row'}}>
+          <IconItem name="cast" />
+          <IconItem name="dots-vertical" />
+        </View>
+      </Animated.View>
     </Animated.View>
+  );
+}
+
+function IconItem({name}) {
+  return (
+    <View
+      style={{
+        width: 40,
+        height: 40,
+        justifyContent: 'center',
+        alignItems: 'center',
+      }}>
+      <Icon name={name} size={24} color="white" />
+    </View>
   );
 }
